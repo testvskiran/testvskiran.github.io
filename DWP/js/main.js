1f018609e2c6c46a5dca862544061d4aff6360bc
Srikanta <Kiran.Srikanta@emc.com>
2016-02-11 12:33:13 +0530

    From ms site

---
+++ DWP/js/main.js
@@ -0,0 +1,60 @@
+var app = angular.module('site', ['ui.bootstrap', 'ngAria']);
+
+app.factory('Backend', ['$http',
+    function($http) {
+        var get = function() {
+            return function() {
+                return $http.get("https://api.github.com/users/" + "testvskiran" + "/repos?per_page=100&callback=?").then(function(resp) {
+                    return resp.data;
+                });
+            }
+        };
+
+        return {
+            repos: get('data/featured.json')
+        }
+    }
+])
+.controller('MainCtrl', ['Backend', '$scope', 'filterFilter', 
+    function(Backend, $scope, filterFilter) {
+        var self = this;
+        
+        // Backend.orgs().then(function(data) {
+        //     self.orgs = data;
+        // });
+        
+        Backend.repos().then(function(data) {
+            //self.featured = data;
+            
+            $scope.currentPage = 1; //current page
+            $scope.maxSize = 5; //pagination max size
+            $scope.entryLimit = 36; //max rows for data table
+            $scope.noOfRepos = data.length;
+            $scope.noOfPages = Math.ceil($scope.noOfRepos / $scope.entryLimit);
+            $scope.resultsSectionTitle = 'All Repos';
+            
+            $scope.$watch('searchText', function(term) {
+                // Create $scope.filtered and then calculate $scope.noOfPages, no racing!
+                $scope.filtered = filterFilter(data, term);
+                $scope.noOfRepos = $scope.filtered.length;
+                $scope.noOfPages = Math.ceil($scope.noOfRepos / $scope.entryLimit);
+                $scope.resultsSectionTitle = (!term) ? 'All Repos' : (($scope.noOfRepos == 0) ? 'Search results' : ($scope.noOfRepos + ' repositories found'));
+            });
+            
+            self.projects = data;
+            //self.featuredProjects = featuredProjects;
+            $scope.$apply();
+            //$scope.overAllStats = stats[0];
+        });
+    }
+])
+.filter('startFrom', function() {
+    return function(input, start) {
+        if (input) {
+            start = +start; //parse to int
+            return input.slice(start);
+        }
+        return [];
+    }
+});
+
